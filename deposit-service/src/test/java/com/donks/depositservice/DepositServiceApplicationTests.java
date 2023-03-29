8a1e7093b9b13fcf25301b7b5c08c70511802956
Franco Ortega <ortegafran97@gmail.com>
2023-03-29 20:49:51 -0300

    deposit-service

---
+++ deposit-service/src/test/java/com/donks/depositservice/DepositServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donks.depositservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
