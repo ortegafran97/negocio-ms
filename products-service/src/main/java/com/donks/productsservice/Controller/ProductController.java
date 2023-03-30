77bcc8325ae74e15e757d63ed51bad3fce3aa680
Franco Ortega <ortegafran97@gmail.com>
2023-03-30 15:38:11 -0300

    Products Controller

--- products-service/src/main/java/com/donks/productsservice/Controller/ProductController.java
+++ products-service/src/main/java/com/donks/productsservice/Controller/ProductController.java
@@ -1,14 +1,46 @@
 package com.donks.productsservice.Controller;
 
+import com.donks.productsservice.Model.Product;
 import com.donks.productsservice.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
     @Autowired
-    ProductService service;
+    ProductService productsService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll(){
+        return ResponseEntity.ok(productsService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Product>> findOne(@PathVariable("id") UUID idProduct){
+        return ResponseEntity.ok(productsService.findById(idProduct));
+    }
+
+    @PostMapping
+    public ResponseEntity<Optional<Product>> save(@RequestBody Product product){
+        return ResponseEntity.ok(productsService.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<Product>> update(@PathVariable("id") UUID id,@RequestBody Product product){
+        product.setId(id);
+        return ResponseEntity.ok(productsService.update(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> delete(UUID id){
+        return ResponseEntity.ok(productsService.delete(id));
+    }
+
 }
