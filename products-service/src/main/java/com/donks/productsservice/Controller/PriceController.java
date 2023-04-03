e215a02dc2dbd6974af8c86fb18dd4aed2d0854b
Franco Ortega <ortegafran97@gmail.com>
2023-04-03 10:10:08 -0300

    Requests Mappings

--- products-service/src/main/java/com/donks/productsservice/Controller/PriceController.java
+++ products-service/src/main/java/com/donks/productsservice/Controller/PriceController.java
@@ -1,13 +1,16 @@
 package com.donks.productsservice.Controller;
 
+import com.donks.productsservice.Exceptions.Classes.NotFoundException;
 import com.donks.productsservice.Model.Price;
 import com.donks.productsservice.Service.PriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.Path;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/price")
@@ -16,7 +19,36 @@ public class PriceController {
     @Autowired
     PriceService priceService;
 
-    public ResponseEntity<List<Price>> findAll(){
+    @GetMapping
+    public ResponseEntity<List<Price>> findAll() {
         return ResponseEntity.ok(priceService.findAll());
     }
+
+    @GetMapping("/{idPrice}")
+    public ResponseEntity<Optional<Price>> findById(@PathVariable("idPrice") UUID idPrice){
+        return ResponseEntity.ok(priceService.findById(idPrice));
+    }
+
+    @GetMapping("/all/{idProduct}")
+    public ResponseEntity<List<Price>> findPricesForProduct(@PathVariable("idProduct") UUID idProduct)
+            throws NotFoundException {
+        return ResponseEntity.ok(priceService.findPricesForProduct(idProduct));
+    }
+
+    @GetMapping("/sale/{idProduct}")
+    public ResponseEntity<List<Price>> findSalePrices(@PathVariable("idProduct") UUID id)
+            throws NotFoundException{
+        return ResponseEntity.ok(priceService.findSalesForProduct(id));
+    }
+
+    @GetMapping("/purchase/{idProduct}")
+    public ResponseEntity<List<Price>> findPurchasePrices(@PathVariable("idProduct") UUID id)
+            throws NotFoundException{
+        return ResponseEntity.ok(priceService.findPurchasesForProduct(id));
+    }
+
+    public ResponseEntity<Price> save(@RequestBody Price price){
+        return ResponseEntity.ok(priceService.save(price));
+    }
+
 }
