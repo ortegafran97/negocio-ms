0be71e73fe58c93160be78fd8adbf8ade08b8667
Franco Ortega <ortegafran97@gmail.com>
2023-05-05 10:21:00 -0300

    Get all products

--- deposit-service/src/test/java/com/donks/depositservice/FeignClients/ProductsFeignClientTest.java
+++ deposit-service/src/test/java/com/donks/depositservice/FeignClients/ProductsFeignClientTest.java
@@ -1,6 +1,7 @@
 package com.donks.depositservice.FeignClients;
 
 import com.donks.depositservice.Model.Product;
+import com.donks.depositservice.Service.ProductsService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +19,24 @@ class ProductsFeignClientTest {
     @Autowired
     ProductsFeignClient client;
 
+    @Autowired
+    ProductsService productsService;
+
 
     @Test
-    public void find_all_products(){
-        List<Product> list = client.findAll();
+    public void update_internal_products(){
+        List<Product> old = productsService.findAll();
 
-        list.forEach(System.out::println);
+        System.out.println("~~~~ Old products ~~~~~");
+        old.forEach(System.out::println);
 
-        assertTrue(list.size()>0);
+        productsService.updateProducts();
+        List<Product> updated = productsService.findAll();
+        System.out.println("~~~~ updated products ~~~~~");
+        updated.forEach(System.out::println);
 
+        assertTrue(old.size()<=updated.size());
     }
+
+
 }
