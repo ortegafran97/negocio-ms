05d3ae8ecc005f8f2767e2f735f1fc7bf6584781
Franco Ortega <ortegafran97@gmail.com>
2023-05-17 17:30:36 -0300

    Scheduled task added

---
+++ deposit-service/src/main/java/com/donks/depositservice/Tasks/UpdateProducts.java
@@ -0,0 +1,18 @@
+package com.donks.depositservice.Tasks;
+
+import com.donks.depositservice.Service.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateProducts {
+
+    @Autowired
+    ProductsService productsService;
+
+    @Scheduled(fixedRate = 15000) // Se ejecuta cada 1 hr (?
+    public void updateProductsTask() {
+        productsService.updateProducts();
+    }
+}
