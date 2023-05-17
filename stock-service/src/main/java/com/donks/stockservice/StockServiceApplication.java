c7ede7ed9eae17f8dd7aef9488392471166453b0
Franco Ortega <ortegafran97@gmail.com>
2023-05-17 17:38:48 -0300

    Warn

--- stock-service/src/main/java/com/donks/stockservice/StockServiceApplication.java
+++ stock-service/src/main/java/com/donks/stockservice/StockServiceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+// TODO: deprecated -> delete service
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
