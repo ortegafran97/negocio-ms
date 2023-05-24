e8b7c6209e05c2b9cca32a676fb55978aea11274
Franco Ortega <ortegafran97@gmail.com>
2023-05-24 16:29:04 -0300

    Find by purchase method added

--- deposit-service/src/main/java/com/donks/depositservice/Repository/DepositRepository.java
+++ deposit-service/src/main/java/com/donks/depositservice/Repository/DepositRepository.java
@@ -1,11 +1,15 @@
 package com.donks.depositservice.Repository;
 
 import com.donks.depositservice.Model.Deposit;
+import com.donks.depositservice.Model.Product;
+import com.donks.depositservice.Model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DepositRepository extends JpaRepository<Deposit, UUID> {
+    Optional<Deposit> findByPurchase(Purchase p);
 }
