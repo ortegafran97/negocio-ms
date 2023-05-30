022c8b7db06fb21d7916d4829a8bb7806a9a67b9
Franco Ortega <ortegafran97@gmail.com>
2023-05-30 16:03:21 -0300

    Find by name

--- deposit-service/src/main/java/com/donks/depositservice/Repository/ProductsRepository.java
+++ deposit-service/src/main/java/com/donks/depositservice/Repository/ProductsRepository.java
@@ -4,8 +4,12 @@ import com.donks.depositservice.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, UUID> {
+    List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByName(String name);
 }
