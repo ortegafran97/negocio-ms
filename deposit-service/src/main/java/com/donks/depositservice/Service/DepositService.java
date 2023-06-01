dbf063735a2a7649e810f45c88709465790b6f45
Franco Ortega <ortegafran97@gmail.com>
2023-06-01 00:14:36 -0300

    update state progress

--- deposit-service/src/main/java/com/donks/depositservice/Service/DepositService.java
+++ deposit-service/src/main/java/com/donks/depositservice/Service/DepositService.java
@@ -2,17 +2,28 @@ package com.donks.depositservice.Service;
 
 import com.donks.depositservice.Model.Deposit;
 import com.donks.depositservice.Model.DepositState;
+import com.donks.depositservice.Model.Product;
+import com.donks.depositservice.Model.Purchase;
 import com.donks.depositservice.Repository.DepositRepository;
+import com.netflix.discovery.converters.Auto;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class DepositService {
     @Autowired
     DepositRepository depositRepository;
+
+    @Autowired
+    ProductsService productsService;
+
+    @Autowired
+    StockService stockService;
 
     public Deposit saveOne(Deposit deposit){
         return depositRepository.save(deposit);
@@ -22,7 +33,22 @@ public class DepositService {
         return depositRepository.findAll();
     }
 
-    public Deposit updateState(UUID idDeposito, DepositState state){
+    public Optional<Deposit> findById(UUID id){
+        return depositRepository.findById(id);
+    }
+
+    public Optional<Deposit> findByPurchase(Purchase p){
+        return depositRepository.findByPurchase(p);
+    }
+
+    public Optional<Deposit> updateState(UUID idDeposito, @NotNull DepositState state){
+
+        Optional<Deposit> exists = depositRepository.findById(idDeposito);
+        if(exists.isEmpty())
+            return Optional.empty();
+
+        Deposit d = exists.get();
+
         switch (state){
             case PENDING:
                 System.out.println("PENDING");
@@ -38,23 +64,35 @@ public class DepositService {
 
             case RETURNED:
                 System.out.println("RETURNED");
+                d.setState(state);
+                depositRepository.save(d);
                 break;
 
             case ACCEPTED:
                 System.out.println("ACCEPTED");
 
                 /*
-                * Aquí debemos actualizar el stock de los productos que están dentro de
-                * la compra
+                * Aquí actualizamos el stock de los productos
+                * que están dentro de la compra
                 */
 
+                Purchase p = d.getPurchase();
+
+                p.getItems()
+                        .forEach( i -> {
+                            System.out.println(i);
+                            //TODO: actualizar ProductStock
+
+                            stockService.setStock(i);
+
+                        });
                 break;
 
             default:
                 System.out.println("Salida por defecto");
         }
 
-        return new Deposit();
+        return Optional.empty();
     }
 
 
