6da5d951e7fbe5615f8873e4352c6397e1e8e42a
Franco Ortega <ortegafran97@gmail.com>
2023-06-13 11:03:59 -0300

    repository decorator

--- products-service/src/main/java/com/donks/productsservice/Repository/ProviderRepository.java
+++ products-service/src/main/java/com/donks/productsservice/Repository/ProviderRepository.java
@@ -2,10 +2,12 @@ package com.donks.productsservice.Repository;
 
 import com.donks.productsservice.Model.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ProviderRepository extends JpaRepository<Provider, UUID> {
     Optional<Provider> findByNameContainingIgnoreCase(String text);
     Optional<Provider> findByName(String name);
