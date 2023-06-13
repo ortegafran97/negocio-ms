1cdbbb699d9c904b215f48e7d975ea71bd360f49
Franco Ortega <ortegafran97@gmail.com>
2023-06-13 11:01:26 -0300

    find by name & containing name

--- products-service/src/main/java/com/donks/productsservice/Repository/ProviderRepository.java
+++ products-service/src/main/java/com/donks/productsservice/Repository/ProviderRepository.java
@@ -3,7 +3,10 @@ package com.donks.productsservice.Repository;
 import com.donks.productsservice.Model.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProviderRepository extends JpaRepository<Provider, UUID> {
+    Optional<Provider> findByNameContainingIgnoreCase(String text);
+    Optional<Provider> findByName(String name);
 }
